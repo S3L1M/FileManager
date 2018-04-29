9776f89e383fcd35e4d8f1e3bbd2917fa78ab4f2
Mohamed Selim <mohamedstoom@gmail.com>
2018-04-29 23:08:56 +0200

    GUI Desgin

---
+++ app/src/main/java/com/example/mohamed/filemanager/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.mohamed.filemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void chmod(View v){
+
+    }
+
+    public void create(View v){
+
+    }
+
+    public void delete(View v){
+
+    }
+
+    public void link(View v){
+
+    }
+}
