88a007df376515897e0be8e7998ce4d2888031a1
Mohamed Selim <mohamedstoom@gmail.com>
2018-04-30 02:16:11 +0200

    Configure Spinner with global variable

--- app/src/main/java/com/example/mohamed/filemanager/MainActivity.java
+++ app/src/main/java/com/example/mohamed/filemanager/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.mohamed.filemanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Boolean isFile = Boolean.TRUE;
+    boolean isFile = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +24,22 @@ public class MainActivity extends AppCompatActivity {
         //There are multiple variations of this, but this is the basic variant.
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         //set the spinners adapter to the previously created one.
+        TextView t = (TextView) findViewById(R.id.lst);
         dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==0)
+                    isFile=true;
+                else
+                    isFile=false;
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Do Nothing
+            }
+        });
     }
 
     public void chmod(View v){
